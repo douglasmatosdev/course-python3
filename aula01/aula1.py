7dc76ad0fad82dfca360c22631f767844873fa4b
douglasmatosdev <douglasmatosdev@gmail.com>
2024-01-28 18:35:48 -0300

    doc string

--- aula01/aula1.py
+++ aula01/aula1.py
@@ -1,3 +1,14 @@
+"""
+    Escrever DocString
+"""
+
+""" Escrever DocString """
+
+'''
+    Escrever DocString
+'''
+''' Escrever DocString '''
+
 # Permite escrever um comentários
 print(123) # Na frente
 #Abaixo
