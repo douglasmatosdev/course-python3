812a1c1d6f46351430c8bd9e8aded88a6c000c4c
douglasmatosdev <douglasmatosdev@gmail.com>
2024-02-01 12:51:06 -0300

    aula3 - print() and strings

---
+++ aula3/aula3.py
@@ -0,0 +1,24 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+print(1234)
+
+# Aspas simples
+print("Douglas Matos")
+print(1, 'Douglas "Matos"')
+
+# Aspas duplas
+print("Douglas Matos")
+print(1, "Douglas 'Matos'")
+
+# Escape
+print('Douglas "Matos"')
+
+# r
+print(r"Douglas \"Matos\"")
+
+print('Explícito', 'é', 'melhor " do que implícito')
