ff5482f1a2a0ede9c97f2ce59813e2b0a52da9e9
douglasmatosdev <douglasmatosdev@gmail.com>
2024-02-02 17:45:40 -0300

    aula15 - função input()

---
+++ aula15/aula15.py
@@ -0,0 +1,11 @@
+# nome = input("Qual o seu nome? ")
+# print(f"O seu nome é {nome=}") # nome='douglas'
+# print(f"O seu nome é {nome}") # douglas
+
+input_1 = input("Digite um número: ")
+input_2 = input("Digite outro número: ")
+
+int_numero_1 = int(input_1)
+int_numero_2 = int(input_2)
+
+print(f"A soma dos números é: {int_numero_1 + int_numero_2}")
