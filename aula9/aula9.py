1bff35fd940d38ce4cfecd98f6dbb36a449b7e7c
douglasmatosdev <douglasmatosdev@gmail.com>
2024-02-02 16:07:38 -0300

    aula9 - operações

---
+++ aula9/aula9.py
@@ -0,0 +1,26 @@
+adicao = 10 + 10
+print("Adição", adicao)
+
+subtracao = 10 - 10
+print("Subtração", subtracao)
+
+multiplicacao = 10 * 10
+print("Multiplicação", multiplicacao)
+
+divisao = 10 / 2.2  # sempre retorna um float
+print("Divisão", divisao)
+
+divisao_inteira = 10 // 2.2
+print("Divisão inteira", divisao_inteira)
+
+exponenciacao = 2**10
+print("Exponenciação", exponenciacao)
+
+modulo = 55 % 2  # resto da divisão
+print("Módulo", modulo)
+
+print(10 % 8 == 0)  # 10 não é multiplo de 8
+print(16 % 8 == 0)  # 16 é multiplo de 8
+print(10 % 2 == 0)  # 10 é par
+print(15 % 2 == 0)  # 15 não é par
+print(16 % 2 == 0)  # 16 é par
