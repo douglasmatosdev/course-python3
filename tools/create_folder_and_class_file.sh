d5a0e864150f7fd4486f23cdd04bf8adddf163db
douglasmatosdev <douglasmatosdev@gmail.com>
2024-02-23 09:27:10 -0300

    update scripts

---
+++ tools/create_folder_and_class_file.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# ccf (Create Class Folder and File)
+
+section_number=$1
+class_number="aula$2"
+
+aulas_folder="./aulas"
+section=$(ls $aulas_folder | grep $section_number)
+mkdir "$aulas_folder/$section/$class_number" && touch "$aulas_folder/$section/$class_number/$class_number.py"
